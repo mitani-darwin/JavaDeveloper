1388cae8fbbab8ae276e8fde64c71e546c74b02c
Toru MITANI <mitani@daisuke2.com>
2019-08-30 15:50:59 +0900

    Servletのポートを追加した。

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 image=`podman image ls | awk '{print $3}' | awk 'NR==2'`
-podman run --name javaDevelopment -p 2222:22 -itd $image
+podman run --name javaDevelopment -p 2222:22 -p 8081:8080 -itd $image
